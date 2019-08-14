592e9416b496dc7a5df70261b53c1a4252a58ce8
Torres-ssf <sergio.uft@gmail.com>
2019-08-14 19:30:28 -0300

    Add test for destroy

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,33 +3,53 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  let(:user) { FactoryBot.create(:user, :with_posts) }
-  let(:comment_params) { FactoryBot.attributes_for(:comment, post_id: user.posts.first) }
+  # let(:user) { FactoryBot.create(:user, :with_posts) }
+  # let(:comment) do
+  #   FactoryBot.create(:comment, post: user.posts.first, user: user)
+  # end
+  # let(:comment_params) { FactoryBot.attributes_for(:comment, post_id: user.posts.first) }
+
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, user: @user)
+    @comment = FactoryBot.create(:comment, user: @user, post: @post)
+    @comment_params = FactoryBot.attributes_for(:comment, post_id: @post)
+  end
+
   describe '#create' do
     context 'as an authenticated user' do
       it 'comment on a post' do
-        sign_in user
+        sign_in @user
         expect do
-          post :create, params: { comment: comment_params }
+          post :create, params: { comment: @comment_params }
           expect(flash[:success]).to be_present
           expect(response).to redirect_to posts_path
-        end.to change(user.comments, :count).by(1)
+        end.to change(@user.comments, :count).by(1)
       end
     end
 
     context 'as a guest' do
       it 'returns a 302 response' do
-        post :create, params: { comment: comment_params }
+        post :create, params: { comment: @comment_params }
         expect(response).to have_http_status '302'
       end
 
       it 'redirects to the sign-in page' do
-        post :create, params: { comment: comment_params }
+        post :create, params: { comment: @comment_params }
         expect(response).to redirect_to '/users/sign_in'
       end
     end
   end
 
   describe '#destroy' do
+    context 'as an authenticated user' do
+      it 'delete a comment' do
+        sign_in @user
+        expect do
+          delete :destroy, params: { id: @comment.id }
+          expect(flash[:success]).to be_present
+        end.to change(@user.comments, :count).by(-1)
+      end
+    end
   end
 end
