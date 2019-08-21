25b8cec2eeacb3d4c6525b03b8d3a18e117c26a4
Torres-ssf <sergio.uft@gmail.com>
2019-08-21 14:24:22 -0300

    Add friendship feature test

---
+++ spec/features/friendships_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Friendships', type: :feature do
+  let(:thor) { FactoryBot.create(:user) }
+  let(:hulk) { FactoryBot.create(:user) }
+  let(:friendship) { FactoryBot.create(:friendship, user_id: thor.id, friend_id: hulk.id) }
+  scenario 'user can send invitation to another user' do
+    sign_in thor
+    visit user_path(hulk)
+    expect do
+      expect(page).to have_content hulk.name
+      expect(page).to have_content 'Send Friend Request'
+      click_button 'Send Friend Request'
+      expect(page).to have_content 'Friend request sent'
+      expect(page). to have_content 'Cancel Friend Request'
+    end
+  end
+
+  scenario 'user can accept friend request' do
+    friendship.save
+    sign_in hulk
+    visit user_path(thor)
+    expect do
+      expect(page).to have_content thor.name
+      expect(page).to have_content 'Accept Friend Request'
+      expect(page).to have_content 'Reject Friend Request'
+      click_button 'Accept Friend Request'
+      expect(page).to have_content 'Unfriend'
+    end
+  end
+
+  scenario 'user can reject friend request' do
+    friendship.save
+    sign_in hulk
+    visit user_path(thor)
+    expect do
+      click_button 'Reject Friend Request'
+      expect(page).to have_content 'Send Friend Request'
+    end
+  end
+
+  scenario 'user can undo the friendship' do
+    friendship.confirm_friend
+    sign_in thor
+    visit user_path(hulk)
+    expect do
+      expect(page).to have_content 'Unfriend'
+      click_button 'Unfriend'
+      expect(page).to have_content 'Send Friend Request'
+    end
+  end
+end
